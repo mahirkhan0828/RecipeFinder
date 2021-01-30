fa58ef4465ff1aea5522b2a8af717a4099f9c8de
Mahir Khan <mahirkhan0828@Mahirs-MacBook-Air.local>
2021-01-30 15:48:32 -0600

    Made skeleton (No API call yet)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import React from 'react';
+import { Itemlist } from '../ItemList/Itemlist';
 
 export class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  
   render(){
     return (
       <div className="App">
         <div className = "header">
           <h1>Recipe Finder</h1>
         </div>
+        <Itemlist></Itemlist>
+
       </div>
     );
   }
---
+++ src/Components/ItemList/Itemlist.js
@@ -0,0 +1,78 @@
+import './Itemlist.css';
+import React from 'react';
+
+export class Itemlist extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        item : "",
+        itemList : [],
+        currId: 1
+    }
+  }
+  updateInput(itemId, itemName){
+        this.setState({
+            [itemId]: itemName
+        })
+  }
+  addFoodItem(){
+      const newItem = {
+          id: this.state.currId,
+          name: this.state.item
+      };
+      this.state.itemList.push(newItem);
+      this.setState({
+          item: "",
+          currId: this.state.currId + 1
+    })
+  }
+
+  deleteFoodItem(id){
+      this.setState({
+          itemList: this.state.itemList.filter(foodItem => foodItem.id !== id)
+      })
+     
+  }
+  retrieveRecipes(){
+    // Will call for API here
+  }
+
+  showRecipeButton(){
+      if (this.state.itemList.length > 0){
+          return (
+                <button onClick = {() => this.retrieveRecipes()}>Find Recipes</button>
+          )
+      }
+  }
+
+  render(){
+    return (
+      <div className = "Itemslist">
+          <div>
+              <h2>Add Food Item:</h2>
+              <input 
+              type = "text"
+              placeholder = "Enter"
+              value = {this.state.item}
+              onChange={e=> this.updateInput("item", e.target.value)}
+              />
+              <button onClick = {() => this.addFoodItem()}>Add</button>
+          </div>
+          <ul>
+              {this.state.itemList.map(foodItem =>{
+                  return (
+                      <ul key = {foodItem.id}>
+                        {foodItem.name}
+                        <button onClick = {() => this.deleteFoodItem(foodItem.id)}>X</button>
+                      </ul>
+                  )
+              }
+              )}
+          </ul>
+          {this.showRecipeButton()}
+          
+      </div>
+    );
+  }
+  
+}
